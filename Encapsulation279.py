fe0cc04c6299239965309d8ee401d64a5810f277
Shaelynn Caltagirone <78838464+Taiilzmf@users.noreply.github.com>
2022-07-27 00:52:40 -0700

    added notes

--- Encapsulation279.py
+++ Encapsulation279.py
@@ -1,14 +1,15 @@
-class Protected:
+class Protected: #this is our protected class
     def __init__(self):
-        self._protectedVar = 5
+        self._protectedVar = 5 #protected class denoted with single _
 
 obj = Protected()
 obj._protectedVar = 7
 print(obj.protectedVar)
+#^this objs get data from protected variable
 
 class Private:
     def __init__(self):
-        self.__privateVar = 32
+        self.__privateVar = 32 #private class denoted with double __
 
     def getPrivate(self):
         print(self.privateVar)
@@ -21,3 +22,4 @@ obj.getPrivate()
 obj.setPrivate(50)
 obj.getPrivate()
         
+#^ these obj's get data from private variable
